f492dd3cb86b7488085632d930f2f093d6115d64
denexapp <mail@denexapp.me>
2025-04-27 20:27:05 +0800

    Add singing to gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,24 +1,24 @@
-/*
- * Copyright (c) Meta Platforms, Inc. and affiliates.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import java.util.Properties
+import java.io.FileInputStream
+
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+val keystoreProperties = Properties()
+keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+
 plugins {
   id("com.android.application")
   id("org.jetbrains.kotlin.android")
 }
 
 android {
+  signingConfigs {
+    create("sign") {
+      keyAlias = keystoreProperties["keyAlias"] as String
+      keyPassword = keystoreProperties["keyPassword"] as String
+      storeFile = file(keystoreProperties["storeFile"] as String)
+      storePassword = keystoreProperties["storePassword"] as String
+    }
+  }
   namespace = "me.denexapp.usbvideocapture"
   compileSdk = 35
 
@@ -41,6 +41,7 @@ android {
     release {
       isMinifyEnabled = false
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+      signingConfig = signingConfigs.getByName("sign")
     }
   }
   compileOptions {
@@ -59,6 +60,14 @@ android {
   buildFeatures {
     viewBinding = true
   }
+
+  packaging {
+    resources {
+      pickFirsts.add("META-INF/LICENSE.md")
+      pickFirsts.add("META-INF/LICENSE-notice.md")
+    }
+  }
+
   ndkVersion = "26.3.11579264"
 }
 
